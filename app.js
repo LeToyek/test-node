2f4070163aab688089596587ca7d54e678b1993f
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-04-10 23:37:21 +0700

    add test endpoint

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.get('/test', (req,res) =>{
+    res.send('test sukses')
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
